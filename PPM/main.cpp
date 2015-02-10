c60009c7eeebbe30baced3314ed400af92d11dd3
Elenesgu <elenesgu@gmail.com>
2015-02-10 10:16:25 +0900

    Use Parallel for

--- PPM/main.cpp
+++ PPM/main.cpp
@@ -1,34 +1,64 @@
 #include"PPM.h"
+#include<ppl.h>
 #include<array>
 #include<vector>
 #include<random>
 #include<chrono>
 #include<iostream>
 #include<fstream>
+#include<vector>
+
+struct Result {
+	int Asize;
+	std::chrono::milliseconds time;
+};
 
 std::ostream& out = std::ofstream("output.txt");
+//std::ostream& out = std::cout;
 auto checkTime = std::chrono::high_resolution_clock::now;
+std::array<std::array<std::vector<Result>, 10U>, 10U > Results;
 
-inline std::string MakeMessage(int slength, int asize, std::chrono::microseconds buildingtime);
+const std::array<char, 4U> Letter = { 'A', 'G', 'C', 'T' };
+const int SourceLengthDiv = 100000;
+const int PatternLengthDiv = 10;
+
+void runalgo(int var);
 
 int main() {
-	const std::array<char, 4U> Letter = { 'A', 'G', 'C', 'T' };
-	const int SourceLengthDiv = 100000;
-	const int PatternLengthDiv = 10;
+	concurrency::array_view<int, 1> view(1000U, 0);
+	auto func = runalgo;
+	concurrency::parallel_for(0, 1000, runalgo);
+
+	for (int i = 0; i < 10; i++) {
+		for (int j = 0; j < 10; j++) {
+			out << 100000 * (i + 1) << "\t" << 10 * (j + 1) << "\n";
+			for each(Result obj in Results[i][j]){
+				out << obj.Asize << "\t" << obj.time.count() << "\n";
+			}
+		}		
+	}
+	/*std::vector<std::string> A;
+	A.push_back("AGA");
+	A.push_back("ACTG");
+	A.push_back("ATAT");
+	A.push_back("TCTGC");
+	toc::PalMatching pm(A);*/
+	return 0;
+}
+
+void runalgo(int var) {
 	std::array<std::string, 10U> sources;
 	std::array<std::string, 10U> patterns;
-
 
 	std::random_device rd;
 	std::mt19937 gen(rd());
 	std::uniform_int_distribution<> sellector(0, 3);
 
-	std::chrono::time_point<std::chrono::system_clock> start, end;
+	std::chrono::time_point<std::chrono::high_resolution_clock> start, end;	
 
-	int SourceLength = 100000;
+	int SourceLength = SourceLengthDiv;
 	int PatternLength = 10;
-
-	/*for (int counter = 0; counter < 10; counter++) {
+	for (int counter = 0; counter < 10; counter++) {
 		for (int i = 0; i < SourceLength; i++) {
 			sources[counter].push_back(Letter[sellector(gen)]);
 		}
@@ -39,38 +69,52 @@ int main() {
 		PatternLength += PatternLengthDiv;
 	}
 	for (int testCase = 0; testCase < 10; testCase++) {
+		Result tmpresult;
 		start = checkTime();
 		toc::PalMatching Palmat(patterns[testCase]);
 		end = checkTime();
-		out << MakeMessage(patterns[testCase].length(), Palmat._ASize, 
-			std::chrono::duration_cast<std::chrono::microseconds>(end - start));
+		/*out << MakeMessage(patterns[testCase].length(), Palmat._ASize,
+		std::chrono::duration_cast<std::chrono::milliseconds>(end - start));*/
 		for (int sourceIndex = 0; sourceIndex < 10; sourceIndex++) {
 			start = checkTime();
 			Palmat.run(sources[sourceIndex]);
 			end = checkTime();
-			out << "RunTime: " << std::chrono::duration_cast<std::chrono::microseconds>(end - start).count() << "us\n";
+			tmpresult.Asize = Palmat._ASize;
+			tmpresult.time = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+			Results[sourceIndex][testCase].push_back(tmpresult);
+			std::cout << var << "\t" << testCase << "\t" << sourceIndex << "\t" << tmpresult.time.count() << "\n";
+			/*out << "RunTime: " << std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count() << "us\n";*/
 		}
-	}*/
-	std::vector<std::string> A;
-	A.push_back("AGA");
-	A.push_back("ACTG");
-	A.push_back("ATAT");
-	A.push_back("TCTGC");
-	toc::PalMatching pm(A);
-
-}
-
-std::string MakeMessage(int slength, int asize, std::chrono::microseconds buildingtime) {
-	static int index = 1;
-	std::string Message;
-	Message = "#";
-	Message += std::to_string(index++);
-	Message += " Target Length: ";
-	Message += std::to_string(slength);
-	Message += " A_Size: ";
-	Message += std::to_string(asize);
-	Message += " Automaton Building Time: ";
-	Message += std::to_string(buildingtime.count());
-	Message += "us\n";
-	return std::string(Message);
+	};
+	//for (int loopcounter = 0; loopcounter < 10; loopcounter++) {
+	//	SourceLength = 100000;
+	//	PatternLength = 10;
+	//	for (int counter = 0; counter < 10; counter++) {
+	//		for (int i = 0; i < SourceLength; i++) {
+	//			sources[counter].push_back(Letter[sellector(gen)]);
+	//		}
+	//		SourceLength += SourceLengthDiv;
+	//		for (int i = 0; i < PatternLength; i++) {
+	//			patterns[counter].push_back(Letter[sellector(gen)]);
+	//		}
+	//		PatternLength += PatternLengthDiv;
+	//	}
+	//	for (int testCase = 0; testCase < 10; testCase++) {
+	//		start = checkTime();
+	//		toc::PalMatching Palmat(patterns[testCase]);
+	//		end = checkTime();
+	//		/*out << MakeMessage(patterns[testCase].length(), Palmat._ASize,
+	//		std::chrono::duration_cast<std::chrono::milliseconds>(end - start));*/
+	//		for (int sourceIndex = 0; sourceIndex < 10; sourceIndex++) {
+	//			start = checkTime();
+	//			Palmat.run(sources[sourceIndex]);
+	//			end = checkTime();
+	//			tmpresult.Asize = Palmat._ASize;
+	//			tmpresult.time = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+	//			Results[sourceIndex][testCase].push_back(tmpresult);
+	//			std::cout << loopcounter << "\t" << testCase << "\t" << sourceIndex << "\t" << tmpresult.time.count() << "\n";
+	//			/*out << "RunTime: " << std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count() << "us\n";*/
+	//		}
+	//	}
+	//}
 }
